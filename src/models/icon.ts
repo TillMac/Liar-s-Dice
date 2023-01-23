96411f8d1ea4a80d78105dcf1553042f42620626
TillMac <danny890308@gmail.com>
2023-01-23 21:31:57 +0800

    chore：建立 icon 物件

---
+++ src/models/icon.ts
@@ -0,0 +1,17 @@
+import { ReactComponent as DiceNum1 } from '../assets/dice-six-faces-1.svg';
+import { ReactComponent as DiceNum2 } from '../assets/dice-six-faces-2.svg';
+import { ReactComponent as DiceNum3 } from '../assets/dice-six-faces-3.svg';
+import { ReactComponent as DiceNum4 } from '../assets/dice-six-faces-4.svg';
+import { ReactComponent as DiceNum5 } from '../assets/dice-six-faces-5.svg';
+import { ReactComponent as DiceNum6 } from '../assets/dice-six-faces-6.svg';
+import { ReactComponent as DiceOnFire } from '../assets/dice-fire.svg';
+
+export const Icon = {
+  DiceNum1,
+  DiceNum2,
+  DiceNum3,
+  DiceNum4,
+  DiceNum5,
+  DiceNum6,
+  DiceOnFire
+};
