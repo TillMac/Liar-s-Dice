11ac7895ce27c6de67771b085dea08fb6cbd0f39
TillMac <danny890308@gmail.com>
2023-01-23 21:30:42 +0800

    chore：移除不必要 code

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import AlignCenter from './components/AlignCenter/AlignCenter';
+import BasicDice from './pages/BasicDice/BasicDice';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <AlignCenter>
+        <h1>yooo, 這是展示頁</h1>
+      </AlignCenter>
+      <BasicDice />
+    </>
+  )
 }
 
 export default App;
